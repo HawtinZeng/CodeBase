a3bc1330b70eb106e927ba58e17682c252d27957
Hawtin Zeng <73981739+HawtinZeng@users.noreply.github.com>
2025-03-29 23:49:58 +0800

    chores: add context

--- willow/src/renderers/WebGLRenderer.ts
+++ willow/src/renderers/WebGLRenderer.ts
@@ -1,9 +1,31 @@
 import { Camera } from "../camera/camera";
 import { Scene } from "../scene/scene";
+import { createCanvasElement } from "../tool/element";
+import { RenderState } from "./renderState";
+
+type RenderParameters = { canvas: HTMLCanvasElement };
 
 export class WebGLRenderer {
+  private currentState: RenderState | undefined;
+  private canvas: HTMLCanvasElement;
+  private gl: WebGL2RenderingContext;
+  constructor(params: RenderParameters) {
+    const { canvas } = params;
+    this.canvas = canvas;
+    const context = canvas.getContext("webgl2");
+    if (!context) {
+      console.error(
+        'canvas.getContext("webgl2") returns null, pls check the webgl2 compatibility'
+      );
+      return;
+    }
+    this.gl = context;
+  }
+
   render(scene: Scene, camera: Camera) {
     // get all objects.
-    // for each objectw, bind buffer and drawArray
+    // for each object, bind buffer and drawArray
+
+    this.currentState = new RenderState();
   }
 }
--- willow/src/renderers/renderState.ts
+++ willow/src/renderers/renderState.ts
@@ -1,25 +1,6 @@
-import { Camera } from "../camera/camera";
-import { Scene } from "../scene/scene";
-
-export class State {
-  lightArray = [];
-  shadowArray = [];
-  constructor(public camera: Camera) {}
-}
-
 export class RenderState {
-  stateMap: WeakMap<Scene, Array<State>>;
-  set(scene: Scene, state: State) {
-    let states = this.stateMap.get(scene);
-    if (!states) {
-      states = [];
-      this.stateMap.set(scene, states);
-    }
-
-    states.push(state);
-  }
-
-  get(scene: Scene) {
-    return this.stateMap.get(scene);
+  clearColor = "#ff0000";
+  setClearColor(c: string) {
+    this.clearColor = c;
   }
 }
