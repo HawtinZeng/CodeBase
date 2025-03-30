67c3601282ff83966731b1073a2e2a09b52af147
Hawtin Zeng <73981739+HawtinZeng@users.noreply.github.com>
2025-03-30 17:58:40 +0800

    feat: OrthoCamera

--- willow/src/camera/camera.ts
+++ willow/src/camera/camera.ts
@@ -1,1 +1,9 @@
-export class Camera {}
+import { Matrix4 } from "../math/Matrix4";
+import { Object3D } from "../objects/object3D";
+
+export class Camera extends Object3D {
+  projectionMatrix: Matrix4 = new Matrix4();
+  get matrixWorldInverse() {
+    return this.matrixWorld.invert();
+  }
+}
